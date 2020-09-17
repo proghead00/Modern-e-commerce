ad76d68bb7c189592cf8f78e47de05dca7c768ed
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2020-09-17 11:54:17 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	// THE API (cloud function) URL
-	baseURL: 'http://localhost:5001/clone-9546b/us-central1/api'
+	baseURL: '<YOUR FIRESTORE CLOUD FUNCTION API LINK>'
 });
 
 export default instance;
