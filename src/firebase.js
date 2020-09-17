a151758ef6f635fac4725900bacca4935189e811
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2020-09-17 11:55:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: 'AIzaSyAhZFl4-GAXaHFXADIXKWbXWZ7bvZ3amKo',
-	authDomain: 'clone-9546b.firebaseapp.com',
-	databaseURL: 'https://clone-9546b.firebaseio.com',
-	projectId: 'clone-9546b',
-	storageBucket: 'clone-9546b.appspot.com',
-	messagingSenderId: '399602234887',
-	appId: '1:399602234887:web:14f212c5b6cbd8e26d9ca2',
-	measurementId: 'G-E9JGXZ9DNL'
-};
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyAhZFl4-GAXaHFXADIXKWbXWZ7bvZ3amKo',
+// 	authDomain: 'clone-9546b.firebaseapp.com',
+// 	databaseURL: 'https://clone-9546b.firebaseio.com',
+// 	projectId: 'clone-9546b',
+// 	storageBucket: 'clone-9546b.appspot.com',
+// 	messagingSenderId: '399602234887',
+// 	appId: '1:399602234887:web:14f212c5b6cbd8e26d9ca2',
+// 	measurementId: 'G-E9JGXZ9DNL'
+// };
+
+<YOUR FIREBASE CONFIG FILE HERE, LIKE THE ONE ABOVE>
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
